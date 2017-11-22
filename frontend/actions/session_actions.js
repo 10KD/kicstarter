dcc919b2ba0d0239e1aaa967bd6156ea9499f46a
KDG <mkim.don@gmail.com>
2017-11-22 14:50:05 -0800

    worked on front_end auth

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,31 @@
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+
+
+
+import * as ApiUtil from '../util/session_api_util';
+
+export const receiveCurrentUser = (currentUser) => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
+
+
+export const receiveErrors = (errors) => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors,
+});
+
+export const login = (user) => dispatch => (
+  ApiUtil.login(user).then(userP => dispatch(receiveCurrentUser(userP)),
+  err => (dispatch(receiveErrors(err.responseJSON))))
+);
+
+export const logout = () => dispatch => (
+  ApiUtil.logout.then(() => dispatch(receiveCurrentUser(null)))
+);
+
+export const signup = (user) => dispatch => (
+  ApiUtil.signup(user).then(userP => dispatch(receiveCurrentUser(userP)),
+  err => (dispatch(receiveErrors(err.responseJSON))))
+);
