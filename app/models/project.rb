90689e32722117226ffe7dbc07223be1a4a57e3b
KDG <mkim.don@gmail.com>
2017-11-27 14:15:15 -0800

    developing project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,15 @@
 class Project < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+
+  validates :user_id, :category_id, :funding_goal, :title,
+  :short_blurb, :funding_end_date, presence: true
+
+  validates :title, uniqueness: true
+
+  belongs_to :user
+
+  def days_left
+    distance_of_time_in_words(Time.now, funding_end_date)
+  end
+
 end
--- db/migrate/20171127214244_create_projects.rb
+++ db/migrate/20171127214244_create_projects.rb
@@ -1,15 +1,21 @@
 class CreateProjects < ActiveRecord::Migration[5.1]
   def change
     create_table :projects do |t|
-      t.integer :category
-      t.string :title
+      t.integer :user_id, null: false
+      t.integer :category_id, null: false
+      t.integer :funding_goal, null: false
       t.string :project_img_url
-      t.string :short_blurb
-      t.integer :funding_duration
-      t.integer :funding_goal
-      t.string :project_details
+      t.string :title, null: false
+      t.string :short_blurb, null: false
+      t.text :project_details
+      t.date :funding_end_date, null: false
 
       t.timestamps
     end
+
+    add_index :projects, :category_id
+    add_index :projects, :user_id
+    add_index :projects, :short_blurb
+    add_index :projects, :title, unique: true
   end
 end
